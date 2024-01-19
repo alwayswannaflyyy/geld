f786da64f4f4f086a955e46f0b97abe6fbcaebd7
alwayswannaflyyy <bezsama0817@gmail.com>
2024-01-19 13:14:19 +0800

    init

--- frontend/src/common/axios/index.js
+++ frontend/src/common/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3002/",
+  baseURL: "https://api5-xp7u.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
