58dba7d6ea0057c20c2c31e81e5fc0f56226b133
alwayswannaflyyy <bezsama0817@gmail.com>
2024-01-19 11:43:45 +0800

    init

--- frontend/src/common/axios/index.js
+++ frontend/src/common/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3002/",
+  baseURL: "https://api2-mrlx.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
